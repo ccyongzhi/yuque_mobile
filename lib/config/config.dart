796eae14bc453bc12a440f05ba812537ae7d4b2d
CYZ <cyz@MBPLR.local>
2020-03-21 12:49:32 +0800

    修改界面，删除无用元素，添加按钮

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -3,6 +3,9 @@ class Config {
     return 'YOUR_OWN_AUTH_TOKEN';
   }
   static String get apiPrefix {
-    return 'https://www.yuque.com';
+    return 'https://www.yuque.com/api/v2';
+  }
+  static String get userAgent {
+    return 'yuque_mobile_client';
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 // import 'dart:io';
 import 'package:http/http.dart' as http;
 import './config/config.dart';
@@ -15,8 +18,6 @@ class MyPageA extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text('MyPageA'),
       ),
       body: Center(
@@ -61,22 +62,13 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: '标题栏'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -85,28 +77,18 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
   String _htmlText = '';
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   void _setHtmlText() async {
-    var url = '${Config.apiPrefix}/api/v2/user';
+    var url = '${Config.apiPrefix}/user';
     var headers = {
       'X-Auth-Token': Config.authToken,
+      'User-Agent': Config.userAgent,
     };
     final response = await http.get(url, headers: headers);
+    final userInfo = jsonDecode(response.body);
     setState(() {
-      _htmlText = response.body;
+      _htmlText = userInfo['data'].toString();
     });
   }
 
@@ -151,31 +133,21 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            CupertinoButton(
+              child: Text('知识库'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  new MaterialPageRoute(
+                    builder: (context) => new MyPageA(),
+                  )
+                );
+              },
             ),
-            GestureDetector(
-                child: Text(
-                  '$_counter',
-                  style: Theme.of(context).textTheme.display1,
-                ),
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      new MaterialPageRoute(
-                          builder: (context) => new MyPageA()));
-                }
-                // onTap: () { Navigator.pushNamed(context, '/a'); }
-                ),
             Text('$_htmlText')
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
